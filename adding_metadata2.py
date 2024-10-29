af911705444ad8df97b2735337789caec678b36a
LARRYKIPKURUI <larrykipkurui12@gmail.com>
2024-10-29 23:51:25 +0300

    added metadata code files

---
+++ adding_metadata2.py
@@ -0,0 +1,26 @@
+from importlib.metadata import metadata
+
+import PyPDF2
+
+
+def add_metadata(input_file, output_file, title, author):
+    pdf_reader = PyPDF2.PdfReader(input_file)
+    pdf_writer = PyPDF2.PdfWriter()
+
+    for page_num in range(len(pdf_reader.pages)):
+        pdf_writer.add_page(pdf_reader.pages[page_num])
+
+    metadata = {
+        '/Title': title,
+        '/Author': author,
+    }
+    pdf_writer.add_metadata(metadata)
+
+    with open(output_file, 'wb') as out:
+        pdf_writer.write(out)
+
+    print(f"PDF File with updated metadata is saved as {output_file}")
+
+
+# use func
+add_metadata('merged.pdf', 'metadata_added.pdf', 'sample file for python coding', 'thisbelarry')
